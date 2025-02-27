7ae86bce94bee65cbee9fc06dec24b2db3106122
Manas Shah <manas.shah@koerber-supplychain.com>
2025-02-27 14:20:48 +0530

    first commit

---
+++ src/main/java/com/example/jenkins_learning_1/JenkinsLearning1Application.java
@@ -0,0 +1,15 @@
+package com.example.jenkins_learning_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsLearning1Application {
+
+
+	public class Hello {
+		public static void main(String[] args) {
+			System.out.println("HEllo jenkins");
+		}
+	}
+}
